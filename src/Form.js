6396f15b6c54c2c1379f55213a45c999731c8c85
Malik Faizan Hameed <malik.faizan18@gmail.com>
2020-12-09 17:22:13 +0500

    getting input value

--- src/Form.js
+++ src/Form.js
@@ -8,12 +8,29 @@ class Form extends Component {
         this.state = {
             cityName: ''
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    componentDidMount() {
+        console.log('Mounted!');
+    }
+
+    componentDidUpdate() {
+        console.log('Updated!');
     }
 
     handleClick() {
-        console.log('Clicked!')
+        console.log(this.state.cityName);
     }
+
+    handleChange(event) {
+        const {name, value} = event.target;
+        this.setState({ [name]: value });
+    }
+
     render() {
+        console.log('Rendered!');
         return (
             <div>
                 <section className="top-banner">
@@ -23,6 +40,8 @@ class Form extends Component {
                                 type="text"
                                 placeholder="Search for a city"
                                 name='cityName'
+                                value={this.state.cityName}
+                                onChange={this.handleChange}
                             />
                             <button onClick={this.handleClick}>SUBMIT</button>
                             <span className="msg"></span>
